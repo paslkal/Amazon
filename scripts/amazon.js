4d5f420be70c325d04b8dc8d8c8e2fc62d480c8e
paslkal <164247200+paslkal@users.noreply.github.com>
2024-06-20 02:08:36 +0300

    Make cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -78,7 +78,15 @@ document.addEventListener('DOMContentLoaded', (_) => {
           quantity:1
         })
       }
-      console.log(cart)
+
+      let cartQuantity = 0
+
+      cart.forEach((item) => {
+        cartQuantity+= item.quantity
+      })
+
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity
     })
   })
 });
