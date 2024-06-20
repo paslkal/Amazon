9f1f7c6f2e7a72b126ce34cddba89a801f41ae9c
paslkal <164247200+paslkal@users.noreply.github.com>
2024-06-20 20:54:37 +0300

    create function formatCurrency in utils/money.js and use it in amazon.js and checkoout.js fix radio selector using matchingProduct.is

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 import { cart, addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
 function updateCartQuantity() {
   let cartQuantity = 0
@@ -49,7 +50,7 @@ document.addEventListener('DOMContentLoaded', (_) => {
         </div>
 
         <div class="product-price">
-          $${(product.priceCents / 100).toFixed(2)}
+          $${formatCurrency(product.priceCents)}
         </div>
 
         <div class="product-quantity-container">
