10530d7e2cf3a9a013687cd6ca3f44bf85c2f503
paslkal <164247200+paslkal@users.noreply.github.com>
2024-06-20 05:00:05 +0300

    message "added to cart" popup

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', (_) => {
 
         <div class="product-spacer"></div>
 
-        <div class="added-to-cart">
+        <div class="added-to-cart js-added-to-cart-${product.id}">
           <img src="images/icons/checkmark.png">
           Added
         </div>
@@ -58,11 +58,29 @@ document.addEventListener('DOMContentLoaded', (_) => {
 
   document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+  const addedMessageTimeouts = {}
   document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     button.addEventListener('click', () => {
-      const productId = button.dataset.productId
+      const {productId} = button.dataset
+
+
+      const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`)
       
-      
+      addedMessage.classList.add('added-to-cart-visible')
+
+      const previousTimeoutId = addedMessageTimeouts[productId]
+      if (previousTimeoutId) {
+        clearTimeout(previousTimeoutId)
+      }
+
+      const timeoutId = setTimeout(() => {
+        setTimeout(() => {
+          addedMessage.classList.remove('added-to-cart-visible')
+        }, 2000)
+      })
+
+      addedMessageTimeouts[productId] = timeoutId
+
       let quantity = Number(document
         .querySelector(`.js-quantity-selector-${productId}`).value)
 
