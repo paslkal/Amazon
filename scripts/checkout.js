5f0d99cabc73a8ca697db7dabc32ca65722bb193
paslkal <164247200+paslkal@users.noreply.github.com>
2024-07-06 22:05:04 +0300

    practice using async function

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,6 +4,19 @@ import { loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 // import '../data/backend-practice.js' 
 
+async function loadPage() {
+  console.log('load page')
+  
+  await loadProductsFetch()
+
+  return 'value3'
+}
+
+loadPage().then((value) => {
+  console.log('next step')
+  console.log(value)
+})
+
 Promise.all([
   loadProductsFetch(),
   new Promise((resolve) => {
