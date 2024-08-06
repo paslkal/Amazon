bf1c06b5698b18f5ef6d0e3c8e2f538c1033d0f3
paslkal <164247200+paslkal@users.noreply.github.com>
2024-08-06 23:00:42 +0300

    Create paths in server to add, remove, update cart

--- dist/scripts/checkout/checkoutHeader.js
+++
@@ -1,5 +0,0 @@
-import { cart } from "../../data/cart-class";
-export function renderCheckoutHeader() {
-    document.querySelector('.js-checkout-header')
-        .innerHTML = cart.calculateCartQuantity().toString();
-}
