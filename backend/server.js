e9647712c6660fcdab16cafb17303f281a536771
paslkal <164247200+paslkal@users.noreply.github.com>
2024-08-09 23:34:41 +0300

    Turn header value from boolean to string

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use('/', (req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', false);  
+  res.setHeader('Access-Control-Allow-Credentials', 'false');  
   // res.setHeader("Access-Control-Allow-Private-Network", true);
   next()
 })
