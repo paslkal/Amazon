9b4441250bc4d9cd663ccdcbb88bdf912fba6f53
paslkal <164247200+paslkal@users.noreply.github.com>
2024-08-15 05:29:25 +0300

    load default env. variables for  host and port

--- src-react/react/getEnv.ts
+++ src-react/react/getEnv.ts
@@ -1,7 +1,20 @@
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import path from 'path';
 
-dotenv.config()
+// TODO: Сделать так, чтобы dotenv работал
+const filePath = path.resolve(__dirname, '.env');
+dotenv.config({ path: filePath });
 
 export default function getEnv() {
-  return {port: process.env['PORT'], host: process.env['HOST']}
+  const port = process.env['PORT'] || 1000;
+  const host = process.env['HOST'] || '127.0.0.1';  
+  
+  if (!process.env['PORT'] || !process.env['HOST']) {
+    console.warn(`
+      Warning: Environment variables for PORT = ${port} or HOST = ${host} are not set.
+      Default values will be used.  
+    `);
+  }
+
+  return { port, host };
 }
